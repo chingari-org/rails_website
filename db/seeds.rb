4df0cb7a6288decdbabd463267df5b7933fadbbe
Jiselle Liu <jiselle03@gmail.com>
2020-08-23 18:55:07 -0700

    Add seed file with different user roles and tiers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all()
+
+PASSWORD = "supersecret" 
+
+admin = User.create( 
+    name: "Sansa Stark", 
+    username: "sansastark",
+    email: "ladysansa@winterfell.gov", 
+    password: PASSWORD,
+    role: 10,
+    tier: 1
+) 
+
+moderator = User.create( 
+    name: "Arya Stark", 
+    username: "aryastark",
+    email: "noone@winterfell.gov", 
+    password: PASSWORD,
+    role: 8,
+    tier: 1
+) 
+
+premium_user = User.create( 
+    name: "Robb Stark", 
+    username: "robbstark",
+    email: "robb@winterfell.gov", 
+    password: PASSWORD,
+    role: 0,
+    tier: 1
+) 
+
+free_user = User.create( 
+    name: "Jon Snow", 
+    username: "jonsnow",
+    email: "jon@winterfell.gov", 
+    password: PASSWORD,
+    role: 0,
+    tier: 0
+) 
