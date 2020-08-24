72dfa4d3722f2c86b2f53380f53728156a60af47
Jiselle Liu <jiselle03@gmail.com>
2020-08-23 18:33:16 -0700

    Fix typo/grammar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,13 @@ class ApplicationController < ActionController::Base
   include SearchHelper
 
   private 
-  	# Returns true iff current member is the user associated 
+  	# Returns true if current member is the user associated 
   	# with the current page
   	def correct_member?
   		@user = User.find(params[:id])
   		if @user != current_user
   			redirect_to root_url 
-  			flash[:danger] = "You don't permission to access this page"
+  			flash[:danger] = "You don't have permission to access this page."
   		end
   	end
 
