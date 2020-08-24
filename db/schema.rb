821f96cb19776406925e83c8a5b4e856ad977c29
Jiselle Liu <jiselle03@gmail.com>
2020-08-23 23:41:17 -0700

    Run migration for Resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180430125208) do
+ActiveRecord::Schema.define(version: 20200824015825) do
 
   create_table "courses", force: :cascade do |t|
     t.string "title", default: "Untitled", null: false
@@ -53,6 +53,16 @@ ActiveRecord::Schema.define(version: 20180430125208) do
     t.index ["course_id"], name: "index_enrollments_on_course_id"
     t.index ["user_id", "course_id"], name: "index_enrollments_on_user_id_and_course_id", unique: true
     t.index ["user_id"], name: "index_enrollments_on_user_id"
+  end
+
+  create_table "resources", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_resources_on_user_id"
   end
 
   create_table "texts", force: :cascade do |t|
