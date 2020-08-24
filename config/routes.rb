9ab260d76ed2f7d6e033b04a86fa7085713c8f4c
Jiselle Liu <jiselle03@gmail.com>
2020-08-23 18:46:55 -0700

    Add routes for Resources

--- config/routes.rb
+++ config/routes.rb
@@ -132,6 +132,8 @@ Rails.application.routes.draw do
     get "/authorize/:user_id", to: "users#authorize"
   end
 
+  resources :resources
+
   # Catch-all route
   get '*path', to: 'static_pages#coming_soon'
 end
